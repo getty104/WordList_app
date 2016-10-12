3b942467573cd8fcf075d374baa853fdf38cff07
Toshifumi Kiyono <hayabusatoshihumi@gmail.com>
2016-10-12 14:19:54 +0900

    Made app

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -5,28 +5,43 @@ class WordsController < ApplicationController
 
   def create
     @word = Word.new(word_params)
-    @word.save
-    redirect_to @word
+    if @word.save
+      redirect_to @word
+    else
+      render 'new'
+    end
   end
 
   def new
   end
 
   def edit
-  end
+   @word = Word.find(params[:id])
+ end
 
-  def show
-    @word = Word.find(params[:id])
-  end
+ def show
+  @word = Word.find(params[:id])
+end
 
-  def update
-  end
+def update
+ @word = Word.find(params[:id])
+ 
+ if @word.update(word_params)
+  redirect_to @word
+else
+  render 'edit'
+end
+end
 
-  def destroy
-  end
+def destroy
+  @word = Word.find(params[:id])
+  @word.destroy
+ 
+  redirect_to words_index_path
+end
 
-  private 
-  def word_params
-    params.require(:word).permit(:number,:title,:text)
-  end
+private 
+def word_params
+  params.require(:word).permit(:number,:title,:text)
+end
 end
